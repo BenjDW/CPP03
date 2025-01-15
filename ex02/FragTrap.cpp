5e264133e40ae3964ea2e402829c814a9d1fd885
vm_ubuntu42 <dewitskibenji@live.fr>
2025-01-14 22:18:49 -0800

    change declaration in cpp file

--- ex02/FragTrap.cpp
+++ ex02/FragTrap.cpp
@@ -6,11 +6,29 @@
 /*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 21:28:57 by bde-wits          #+#    #+#             */
-/*   Updated: 2025/01/14 22:11:44 by bde-wits         ###   ########.fr       */
+/*   Updated: 2025/01/14 22:17:54 by bde-wits         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
+
+void	FragTrap::highFivesGuys(void)
+{
+	std::cout << "High five! Up top! Don't leave me hanging, bro!" << std::endl;
+}
+
+FragTrap::FragTrap(std::string nam) : ClapTrap(nam)
+{
+	std::cout << "FragTrap constructor is called" << std::endl;
+	this->Hit_point = 100;
+	this->Energy_point = 100;
+	this->Attack_damage = 30;
+}
+
+FragTrap::~FragTrap()
+{
+	std::cout << "FragTrap le Destructor is called" << std::endl;
+}
 
 FragTrap&	FragTrap::operator=(const FragTrap cpy)
 {
--- ex02/FragTrap.hpp
+++ ex02/FragTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 21:29:03 by bde-wits          #+#    #+#             */
-/*   Updated: 2025/01/14 22:11:48 by bde-wits         ###   ########.fr       */
+/*   Updated: 2025/01/14 22:18:00 by bde-wits         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,5 @@ class FragTrap : public ClapTrap
 		void	highFivesGuys(void);
 	private:
 };
-
-void	FragTrap::highFivesGuys(void)
-{
-	std::cout << "High five! Up top! Don't leave me hanging, bro!" << std::endl;
-}
-
-FragTrap::FragTrap(std::string nam) : ClapTrap(nam)
-{
-	std::cout << "FragTrap constructor is called" << std::endl;
-	this->Hit_point = 100;
-	this->Energy_point = 100;
-	this->Attack_damage = 30;
-}
-
-FragTrap::~FragTrap()
-{
-	std::cout << "FragTrap le Destructor is called" << std::endl;
-}
 
 #endif
